5035b36907ee23aee2c04e3a59fd0cbbc1c8363d
maxcerecedadev <cereceda1991@gmail.com>
2025-10-24 23:09:19 -0500

    feat: add more time to rounds

--- src/domain/entities/TournamentRoom.js
+++ src/domain/entities/TournamentRoom.js
@@ -47,7 +47,7 @@ export class TournamentRoom {
     this.lastBets = new Map();
     this.gameState = GAME_STATES.BETTING;
     this.currentRound = 1;
-    this.maxRounds = 2; //! Máximo número de rondas
+    this.maxRounds = 10; //! Máximo número de rondas
     this.timeRemaining = 30; //!! 60s Luego
     this.manualMode = false;
     this.rouletteEngine = new RouletteEngine(20);
